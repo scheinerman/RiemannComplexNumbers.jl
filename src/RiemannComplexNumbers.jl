6c990ca769a5420cef86892ad28f393ef23042bb
Ed Scheinerman <ers@jhu.edu>
2015-01-17 23:27:31 -0500

    Some aesthetic changes to the code

--- src/RiemannComplexNumbers.jl
+++ src/RiemannComplexNumbers.jl
@@ -1,9 +1,6 @@
 # This package redefines Complex operations so that there is a single
 # complex infinity and a single complex NaN.
 
-# import Base./
-
-
 module RiemannComplexNumbers
 
 import Base.inv, Base.Complex, Base.complex_show, Base.show, Base.showcompact
@@ -13,33 +10,25 @@ export ComplexNaN, ComplexInf, inv
 const ComplexNaN = Complex(NaN,NaN)
 const ComplexInf = Complex(Inf,Inf)
 
-
 function Complex(x::Real, y::Real)
     if isnan(x) || isnan(y)
         return ComplexNaN
     end
-
     if isinf(x) || isinf(y)
         return ComplexInf
     end
     return Complex(promote(x,y)...)
 end
 
-
-## show(io::IO, z::Complex) = complex_show(io, z, false)
-## showcompact(io::IO, z::Complex) = complex_show(io, z, true)
-
 function show(io::IO, z::Complex)
     if isnan(z)
         print(io, "ComplexNaN")
         return
     end
-
     if isinf(z)
         print(io, "ComplexInf")
         return
     end
-
     complex_show(io,z,false)
 end
 
@@ -48,12 +37,10 @@ function showcompact(io::IO, z::Complex)
         print(io, "C_NaN")
         return
     end
-
     if isinf(z)
         print(io, "C_Inf")
         return
     end
-
     complex_show(io,z,true)
 end
 
@@ -62,12 +49,10 @@ end
 # Addition
 
 function +(w::Complex, z::Complex)
-
     w,z = promote(w,z)
     if isnan(w) || isnan(z)
         return ComplexNaN
     end
-
     if isinf(w)
         if isinf(z)
             return ComplexNaN
@@ -77,7 +62,6 @@ function +(w::Complex, z::Complex)
     if isinf(z)
         return ComplexInf
     end
-
     return Complex(w.re+z.re, w.im+z.im)
 end
 
@@ -87,12 +71,10 @@ end
 # Binary minus
 
 function -(w::Complex, z::Complex)
-
     w,z = promote(w,z)
     if isnan(w) || isnan(z)
         return ComplexNaN
     end
-
     if isinf(w)
         if isinf(z)
             return ComplexNaN
@@ -102,7 +84,6 @@ function -(w::Complex, z::Complex)
     if isinf(z)
         return ComplexInf
     end
-
     return Complex(w.re-z.re, w.im-z.im)
 end
 
@@ -115,11 +96,9 @@ function -(w::Complex)
     if isnan(w)
         return ComplexNan
     end
-
     if isinf(w)
         return ComplexInf
     end
-
     return Complex(-w.re, -w.im)
 end
 
@@ -127,49 +106,40 @@ end
 
 function *(w::Complex, z::Complex)
     w,z = promote(w,z)
-
     if isnan(w) || isnan(z)
         return ComplexNaN
     end
-
     if isinf(w)
         if z==0
             return ComplexNaN
         end
         return ComplexInf
     end
-
     if isinf(z)
         if w==0
             return ComplexNaN
         end
         return ComplexInf
     end
-
     return Complex(w.re*z.re - w.im*z.im , w.re*z.im + w.im*z.re)
-
 end
 
 *(w::Complex, x::Real) = w * Complex(x)
 *(x::Real, w::Complex) = Complex(x) * w
 
-#Division
+# Division
 
 function /(w::Complex, z::Complex)
-
     w,z = promote(w,z)
-
     if isnan(w) || isnan(z)
         return ComplexNaN
     end
-
     if isinf(z)
         if isinf(w)
             return ComplexNaN
         end
         return zero(typeof(z))
     end
-
     if z==0
         if w==0
             return ComplexNaN
@@ -179,31 +149,24 @@ function /(w::Complex, z::Complex)
 
     zx = z.re
     zy = z.im
-
     top = w*conj(z)
     d   = zx*zx + zy*zy
-
     return Complex(top.re/d, top.im/d)
-
 end
 
 /(w::Complex, x::Real) = w/Complex(x)
 /(x::Real, z::Complex) = Complex(x)/z
 
-
 function my_inv(z::Complex)
     if isnan(z)
         return ComplexNaN
     end
-
     if isinf(z)
         return zero(z)
     end
-
     if z == 0
         return ComplexInf
     end
-
     return conj(z)/abs2(z)
 end
 
@@ -211,7 +174,6 @@ end
 inv(w::Complex{Float64}) = my_inv(w)
 inv(w::Complex{Integer}) = my_inv(w)
 inv(w::Complex) = my_inv(w)
-
 
 # Equality
 
@@ -222,7 +184,6 @@ function ==(w::Complex, z::Complex)
     if isinf(w) && isinf(z)
         return true
     end
-
     return w.re == z.re && w.im == z.im
 end
 
