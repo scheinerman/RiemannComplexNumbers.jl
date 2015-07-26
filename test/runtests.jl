d06389c637767e89ea70d8a0f229cb57f7e88461
Ed Scheinerman <ers@jhu.edu>
2015-07-26 11:51:46 -0400

    Added a few tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,8 @@ b = 4-5im
 @test a+b == 6-2im
 @test a/0 == ComplexInf
 @test isnan((a-a)/(b-b)) 
+@test Inf + im == Inf + 2*im
+@test ComplexInf * ComplexInf == ComplexInf
+@test isnan(ComplexInf + ComplexInf)
+@test isnan(0*ComplexInf)
+
